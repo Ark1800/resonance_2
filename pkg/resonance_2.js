873f3f860f2db0e19240d9e2393d795a62f5d2ac
Ark1800 <andrewstephencampbell@gmail.com>
2026-06-09 21:42:19 -0400

    commit 7.1.2

--- pkg/resonance_2.js
+++ pkg/resonance_2.js
@@ -204,8 +204,8 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function wasm_bindgen__convert__closures_____invoke__hdb9223d12a9daa3a(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__hdb9223d12a9daa3a(arg0, arg1, arg2);
+function wasm_bindgen__convert__closures_____invoke__ha6d1a929fc3ce4dc(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__ha6d1a929fc3ce4dc(arg0, arg1, arg2);
 }
 
 const __wbindgen_enum_RequestMode = ["same-origin", "no-cors", "cors", "navigate"];
@@ -366,9 +366,9 @@ function __wbg_get_imports() {
         const ret = getStringFromWasm0(arg0, arg1);
         return ret;
     };
-    imports.wbg.__wbindgen_cast_d4e72ce7b74d162f = function(arg0, arg1) {
-        // Cast intrinsic for `Closure(Closure { dtor_idx: 192, function: Function { arguments: [Externref], shim_idx: 193, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-        const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hb4a1aa1b765ed5a1, wasm_bindgen__convert__closures_____invoke__hdb9223d12a9daa3a);
+    imports.wbg.__wbindgen_cast_c8eb7806e6307384 = function(arg0, arg1) {
+        // Cast intrinsic for `Closure(Closure { dtor_idx: 187, function: Function { arguments: [Externref], shim_idx: 188, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+        const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h6c4bffc15d8607de, wasm_bindgen__convert__closures_____invoke__ha6d1a929fc3ce4dc);
         return ret;
     };
     imports.wbg.__wbindgen_init_externref_table = function() {
